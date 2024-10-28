fb7b13145b3c704480d952ea1e5cf428ee0ee8fe
Juan Pedro Medina Silva <64823850+juanpemedina@users.noreply.github.com>
2024-10-27 22:30:58 -0600

    Cuarta-Transformación

--- gilded_rose/lib/gilded_rose.ex
+++ gilded_rose/lib/gilded_rose.ex
@@ -3,9 +3,14 @@ defmodule GildedRose do
     Enum.map(items, &update_item/1)
   end
 
-  defp decrease_quality(%{quality: quality, name: "Sulfuras, Hand of Ragnaros"} = item), do: item
   defp decrease_quality(%{quality: quality} = item) when quality > 0, do: %{item | quality: quality - 1}
   defp decrease_quality(item), do: item
+
+  defp decrease_quality_expired(item) do
+    item
+    |> decrease_quality()
+    |> decrease_quality()
+  end
 
   defp increase_quality(%{quality: quality} = item) when quality < 50, do: %{item | quality: quality + 1}
   defp increase_quality(item), do: item
@@ -18,21 +23,18 @@ defmodule GildedRose do
   end
 
   defp handle_expired_item(%{name: "Aged Brie"} = item), do: increase_quality(item)
-  
   defp handle_expired_item(%{name: "Backstage passes to a TAFKAL80ETC concert"} = item), do: %{item | quality: 0}
-  
-  defp handle_expired_item(%{name: "Sulfuras, Hand of Ragnaros"} = item), do: item
+  defp handle_expired_item(item), do: decrease_quality_expired(item)
 
-  defp handle_expired_item(item) when item.quality > 0, do: decrease_quality(item)
-  defp handle_expired_item(item), do: item
+  def update_item(%{name: "Sulfuras, Hand of Ragnaros"} = item), do: item
 
   def update_item(item) do
-    item = cond do
-      item.name == "Aged Brie" -> increase_quality(item)
-      item.name == "Backstage passes to a TAFKAL80ETC concert" -> update_backstage_pass_quality(item)
-      item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" -> decrease_quality(item)
-      true -> item
-    end
+    item =
+      cond do
+        item.name == "Aged Brie" -> increase_quality(item)
+        item.name == "Backstage passes to a TAFKAL80ETC concert" -> update_backstage_pass_quality(item)
+        true -> decrease_quality(item)
+      end
 
     item = if item.name != "Sulfuras, Hand of Ragnaros", do: %{item | sell_in: item.sell_in - 1}, else: item
 
