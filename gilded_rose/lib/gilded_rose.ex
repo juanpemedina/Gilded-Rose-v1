518acaf3e527ab4dee32acc99197ae2fc5946030
Juan Pedro Medina Silva <64823850+juanpemedina@users.noreply.github.com>
2024-10-27 22:15:32 -0600

    Segunda-transformación

--- gilded_rose/lib/gilded_rose.ex
+++ gilded_rose/lib/gilded_rose.ex
@@ -7,44 +7,42 @@ defmodule GildedRose do
   defp decrease_quality(%{quality: quality} = item) when quality > 0, do: %{item | quality: quality - 1}
   defp decrease_quality(item), do: item
 
+  defp increase_quality(%{quality: quality} = item) when quality < 50 do
+    %{item | quality: quality + 1}
+  end
+  defp increase_quality(item), do: item
+
+  defp update_backstage_pass_quality(item) do
+    item
+    |> increase_quality()
+    |> (fn item ->
+          if item.sell_in < 11, do: increase_quality(item), else: item
+        end).()
+    |> (fn item ->
+          if item.sell_in < 6, do: increase_quality(item), else: item
+        end).()
+  end
+
   def update_item(item) do
     item = cond do
+      item.name == "Aged Brie" ->
+        increase_quality(item)
+        
+      item.name == "Backstage passes to a TAFKAL80ETC concert" ->
+        update_backstage_pass_quality(item)
+      
       item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
         decrease_quality(item)
-      true ->
-        cond do
-          item.quality < 50 ->
-            item = %{item | quality: item.quality + 1}
-            cond do
-              item.name == "Backstage passes to a TAFKAL80ETC concert" ->
-                item = cond do
-                  item.sell_in < 11 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-                cond do
-                  item.sell_in < 6 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-              true -> item
-            end
-          true -> item
-        end
+        
+      true -> item
     end
+    
     item = cond do
       item.name != "Sulfuras, Hand of Ragnaros" ->
         %{item | sell_in: item.sell_in - 1}
       true -> item
     end
+    
     cond do
       item.sell_in < 0 ->
         cond do
