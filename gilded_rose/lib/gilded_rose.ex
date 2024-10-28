929aac5c3f6bfa8ef3ba377229930713a66ba30d
Juan Pedro Medina Silva <64823850+juanpemedina@users.noreply.github.com>
2024-10-27 22:41:40 -0600

    Sexta-transformación

--- gilded_rose/lib/gilded_rose.ex
+++ gilded_rose/lib/gilded_rose.ex
@@ -33,15 +33,20 @@ defmodule GildedRose do
   def update_item(%{name: "Sulfuras, Hand of Ragnaros"} = item), do: item
 
   def update_item(item) do
-    item =
-      cond do
-        item.name == "Aged Brie" -> increase_quality(item)
-        item.name == "Backstage passes to a TAFKAL80ETC concert" -> update_backstage_pass_quality(item)
-        true -> decrease_quality(item)
-      end
-
-    item = if item.name != "Sulfuras, Hand of Ragnaros", do: %{item | sell_in: item.sell_in - 1}, else: item
-
+    item = update_quality_based_on_name(item)
+    item = update_sell_in(item)
     if item.sell_in < 0, do: handle_expired_item(item), else: item
+  end
+
+  defp update_quality_based_on_name(item) do
+    cond do
+      item.name == "Aged Brie" -> increase_quality(item)
+      item.name == "Backstage passes to a TAFKAL80ETC concert" -> update_backstage_pass_quality(item)
+      true -> decrease_quality(item)
+    end
+  end
+
+  defp update_sell_in(item) do
+    if item.name != "Sulfuras, Hand of Ragnaros", do: %{item | sell_in: item.sell_in - 1}, else: item
   end
 end
